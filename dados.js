c4e396f361273c8a6e51f070af6f10e984cfb6b7
Adaptare01 <adaptaresoftware@gmail.com>
2024-09-09 11:27:42 -0300

    Update dados.js inclusão de tags na lista

--- dados.js
+++ dados.js
@@ -8,6 +8,7 @@ let vegetais = [
     descricao:
       "Rico em vitaminas C e K, o brócolis é conhecido por suas propriedades anticancerígenas e antioxidantes.",
     link: "https://pt.wikipedia.org/wiki/Br%C3%B3colis",
+    tag: "Vitamina K  Folha  Médio  Amargo  Verde  Inverno",
   },
   {
     nome: "Couve-flor",
@@ -18,6 +19,7 @@ let vegetais = [
     descricao:
       "A couve-flor é uma excelente fonte de vitamina C e fibras, além de conter compostos que ajudam a fortalecer o sistema imunológico.",
     link: "https://pt.wikipedia.org/wiki/Couve-flor",
+    tag: "Vitamina C  Flor  Médio  Leve  Branco  Inverno",
   },
   {
     nome: "Cenoura",
@@ -28,6 +30,7 @@ let vegetais = [
     descricao:
       "Rica em betacaroteno, a cenoura é essencial para a saúde da visão e um antioxidante poderoso.",
     link: "https://pt.wikipedia.org/wiki/Cenoura",
+    tag: "Vitamina A  Raiz  Pequeno  Doce  Laranja  Outono",
   },
   {
     nome: "Pimentão",
@@ -38,6 +41,7 @@ let vegetais = [
     descricao:
       "O pimentão é uma excelente fonte de vitamina C e antioxidantes, que ajudam a fortalecer o sistema imunológico.",
     link: "https://pt.wikipedia.org/wiki/Piment%C3%A3o",
+    tag: "Vitamina C  Fruto  Médio  Doce/Picante  Variado  Verão",
   },
   {
     nome: "Abóbora",
@@ -48,6 +52,7 @@ let vegetais = [
     descricao:
       "Rica em betacaroteno, a abóbora é uma ótima fonte de vitamina A e fibras, contribuindo para a saúde da visão e digestão.",
     link: "https://pt.wikipedia.org/wiki/Ab%C3%B3bora",
+    tag: "Vitamina A  Fruto  Grande  Doce  Laranja  Outono",
   },
   {
     nome: "Chuchu",
@@ -58,6 +63,7 @@ let vegetais = [
     descricao:
       "O chuchu é uma excelente fonte de água e fibras, auxiliando na hidratação e digestão.",
     link: "https://pt.wikipedia.org/wiki/Chuchu",
+    tag: "Vitamina C  Fruto  Grande  Leve  Verde  Verão",
   },
   {
     nome: "Mandioca",
@@ -68,6 +74,7 @@ let vegetais = [
     descricao:
       "A mandioca é uma boa fonte de carboidratos complexos e fibras, fornecendo energia de forma gradual.",
     link: "https://pt.wikipedia.org/wiki/Mandioca",
+    tag: "Carboidratos  Raiz  Grande  Neutro  Marrom  Todo o ano",
   },
   {
     nome: "Alho",
@@ -78,6 +85,7 @@ let vegetais = [
     descricao:
       "O alho é conhecido por suas propriedades antibacterianas e antifúngicas, além de ser rico em antioxidantes.",
     link: "https://pt.wikipedia.org/wiki/Alho",
+    tag: "Alicina  Bulbo  Pequeno  Forte  Branco  Inverno",
   },
   {
     nome: "Cebola",
@@ -88,6 +96,7 @@ let vegetais = [
     descricao:
       "Rica em antioxidantes, a cebola ajuda a fortalecer o sistema imunológico e possui propriedades anti-inflamatórias.",
     link: "https://pt.wikipedia.org/wiki/Cebola",
+    tag: "Quercetina  Bulbo  Médio  Forte  Amarelo  Inverno",
   },
   {
     nome: "Repolho",
@@ -98,6 +107,7 @@ let vegetais = [
     descricao:
       "O repolho é uma excelente fonte de vitamina C e fibras, além de conter compostos que ajudam a prevenir o câncer.",
     link: "https://pt.wikipedia.org/wiki/Repolho",
+    tag: "Vitamina C  Folha  Grande  Levemente amargo  Verde  Outono",
   },
   {
     nome: "Espinafre",
@@ -108,6 +118,7 @@ let vegetais = [
     descricao:
       "Rico em ferro e vitamina K, o espinafre é essencial para a saúde das células sanguíneas e dos ossos.",
     link: "https://pt.wikipedia.org/wiki/Espinafre",
+    tag: "Ferro  Folha  Pequeno  Levemente amargo  Verde escuro  Primavera",
   },
   {
     nome: "Cebolinha",
@@ -118,6 +129,7 @@ let vegetais = [
     descricao:
       "A cebolinha é rica em vitamina C e antioxidantes, com um sabor levemente picante.",
     link: "https://pt.wikipedia.org/wiki/Cebolinha",
+    tag: "Vitamina K  Folha  Pequeno  Picante  Verde  Primavera",
   },
   {
     nome: "Salsinha",
@@ -128,6 +140,7 @@ let vegetais = [
     descricao:
       "A salsinha é rica em vitamina K e beta-caroteno, além de ter propriedades anti-inflamatórias.",
     link: "https://pt.wikipedia.org/wiki/Salsinha",
+    tag: "Vitamina K  Folha  Pequeno  Picante  Verde  Primavera",
   },
   {
     nome: "Abobrinha",
@@ -138,6 +151,7 @@ let vegetais = [
     descricao:
       "A abobrinha é rica em água e fibras, sendo uma ótima opção para uma dieta saudável e equilibrada.",
     link: "https://pt.wikipedia.org/wiki/Abobrinha",
+    tag: "Vitamina C  Fruto  Médio  Leve  Verde  Verão",
   },
   {
     nome: "Tomate",
@@ -145,7 +159,9 @@ let vegetais = [
     espacamentoLinha: 80,
     espacamentoEntre: 50,
     tempoColheita: 60,
-    descricao: "O tomate é uma fruta rica em licopeno, um antioxidante que ajuda a prevenir diversas doenças.",
-    link: "https://pt.wikipedia.org/wiki/Tomate"
-}
+    descricao:
+      "O tomate é uma fruta rica em licopeno, um antioxidante que ajuda a prevenir diversas doenças.",
+    link: "https://pt.wikipedia.org/wiki/Tomate",
+    tag: "Vitamina C  Fruto  Médio  Adocicado  Vermelho  Verão",
+  },
 ];
